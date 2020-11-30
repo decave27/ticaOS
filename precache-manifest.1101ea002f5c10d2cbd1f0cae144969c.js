3513bf75ebc82852a289af44722d89f0aca187c5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-30 00:41:18 +0000

    Deploy thoratica/ticaOS to github.com/thoratica/ticaOS.git:gh-pages

---
+++ precache-manifest.1101ea002f5c10d2cbd1f0cae144969c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d500abc1f8327fe56fad514018ba1747",
+    "url": "/ticaOS/index.html"
+  },
+  {
+    "revision": "97c50142c879efeb90eb",
+    "url": "/ticaOS/static/css/main.3b547580.chunk.css"
+  },
+  {
+    "revision": "9d1ccc48d3ba9061ecf3",
+    "url": "/ticaOS/static/js/2.037b5cfd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ticaOS/static/js/2.037b5cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97c50142c879efeb90eb",
+    "url": "/ticaOS/static/js/main.347c985b.chunk.js"
+  },
+  {
+    "revision": "279af31fecf1af2b3638",
+    "url": "/ticaOS/static/js/runtime-main.30a35ffb.js"
+  }
+]);
